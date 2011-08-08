e0ee14ee94674343b969177ab9941c2b0d82af37
Eric Bakan <bakanatorz@gmail.com>
2011-08-07 18:52:16 -0700

    Added GPL Headers

--- fieldmap.h
+++ fieldmap.h
@@ -1,9 +1,25 @@
+/*
+ *  fieldmap.h
+ *  Copyright (C) 2011 Eric Bakan
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
-  * gui.c
-  * Written by Eric Bakan
-  * Dimensions of and array
-  * representing the fieldmap
+  * Dimensions of and array representing the field map
   */
+
 const uint16_t fieldwidth=63;
 const uint16_t fieldheight=63;
 /*
